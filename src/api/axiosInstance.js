b7fa295ffe164759b53129216fa47d7d49a6a1f0
Rishal-KV <rishkv923@gmail.com>
2024-08-27 23:50:01 +0530

    first commit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : 'http://localhost:3000'
+    baseURL : 'https://jobapp.nutrix.fun'
 })
 
 axiosInstance.interceptors.request.use(
