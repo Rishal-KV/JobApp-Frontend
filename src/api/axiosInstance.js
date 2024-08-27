fde094a6984b865a7b6753ee86f10f44fb301e63
Rishal-KV <rishkv923@gmail.com>
2024-08-27 22:18:18 +0530

    UI changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : 'https//blogapp.nutrix.fun'
+    baseURL : 'https://jobapp.nutrix.fun'
 })
 
 axiosInstance.interceptors.request.use(
